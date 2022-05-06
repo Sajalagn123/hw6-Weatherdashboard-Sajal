7d08438e86740342c63e54e4a5643febba342fa2
Sajal Agnihotri <sajalagn3@gmail.com>
2022-05-05 23:43:58 -0700

    file update 4

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Step 1: Have current and future weather conditions for the city present and added to search history
-// Step 2: Have city name, date, weather icon, temperature, humidity, wind speed, and UV index displayed when current weather conditions for city is viewed
-// Step 3: Have UV index that indicates if weather conditions are favorable, moderate, or severe
-// Step 4: Have 5-day forecast that diaplays future conditions for the city, including weather icon, date, temperature, wind speed, and humidity
-// Step 5: Have present and future conditions for city displayed when user clicks on city in search history
-
-/** Refreshes the weather data */
 var results;
 var city_name;
 
